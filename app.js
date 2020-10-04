c4228f87ed3400004f1538ecbe60217f184fc03d
GTeague <developer@ableant.com>
2020-10-04 18:47:03 -0500

    Allow CORS. tweak api endpoint to plural.

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ conn.connect(function (err) {
   console.log("You are now connected with the store.");
 });
 
+//Allow CORS -
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyparser.json());
 app.use(
   bodyparser.urlencoded({
@@ -22,7 +32,7 @@ app.use(
   })
 );
 
-app.get("/device", (req, res) => {
+app.get("/devices", (req, res) => {
   console.log("Received a GET method.");
   conn.query("select * from tblDevices", function (error, results, fields) {
     if (error) throw error;
@@ -30,11 +40,11 @@ app.get("/device", (req, res) => {
   });
 });
 
-app.post("/device", (req, res) => {
+app.post("/devices", (req, res) => {
   console.log("Received a POST HTTP method");
   var params = req.body;
   console.log(params);
-  conn.query("INSERT INTO customer SET ?", params, function (
+  conn.query("INSERT INTO tblDevices SET ?", params, function (
     error,
     results,
     fields
@@ -44,13 +54,13 @@ app.post("/device", (req, res) => {
   });
 });
 
-app.put("/", (req, res) => {
+app.put("/devices", (req, res) => {
   return res.send("Received a PUT HTTP method");
 });
 
-app.delete("/", (req, res) => {
+app.delete("/devices", (req, res) => {
   return res.send("Received a DELETE HTTP method");
 });
 
-app.listen(process.env.port || 3000);
-console.log("Web Server is listening at port " + (process.env.port || 3000));
+app.listen(process.env.port || 3001);
+console.log("Web Server is listening at port " + (process.env.port || 3001));
